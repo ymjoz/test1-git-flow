979e52fe7d8a85bacd4987be46acbb481bd265c7
ymjoz <11111148@art365.nuta.edu.tw>
2024-06-08 10:23:41 +0800

    test small add

--- another_module1.py
+++ another_module1.py
@@ -1,6 +1,8 @@
 # Description: This is another module that imports my_module
 
-import my_module
+import my_module, add_module
 
 print(my_module.greeting('OZ'))
+
+print(add_module.addMe(99,22.3))
 
