0f7a9d49da315e15c45924c2e700a1e1eca3183b
ymjoz <11111148@art365.ntua.edu.tw>
2024-06-08 18:52:23 +0800

    learn unpacking

--- info.py
+++ info.py
@@ -4,6 +4,10 @@ my_dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
 
 my_dic1 = {'name': 'John', 'age': 25, 'city': 'New York'}
 
+boeing, airbus, embraer = ['747', 'A380', 'Phenom 300'] # sequence unpacking
+tesla, honda, bmw = {'tesla': 'model s', 'honda': 'civic', 'bmw': 'x5'}.values() # iterable unpacking
+vehicle_model = ('bus', 'train', 'plane') # tuple unpacking
+
 
 def my_func():
     print(my_dic1['city'])
@@ -17,4 +21,4 @@ def my_func():
 
 
 if __name__ == '__main__':
-    print('This is the main program')
+    print('This is info.py')
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 # Description: This is another module that imports my_module
 
-import add_module
-import feature3_2 
+import info 
 import pkg1.module1 as module1
 import pkg1.module2 as module2
 
 
 
-car = module1.AutoCar('tesla')
-car.run()
+teslaCar = module1.AutoCar('tesla')
+teslaCar.run()
 
 cb650 = module2.Scooter('red', 'cb650')
 cb650.drive()
 
-module1.test('jack')
+module1.test('孫女訪問中')
+
+
+print(info.tesla)
+
+print(info.embraer)
+
+first, second, third = info.vehicle_model
+print(first, second, third)
