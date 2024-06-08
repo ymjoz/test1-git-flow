7497b20725dec7db3f645dd97cf1dee0c4687f95
ymjoz <11111148@art365.nuta.edu.tw>
2024-06-08 09:40:54 +0800

    small add

--- feature3.2.py
+++ feature3.2.py
@@ -2,6 +2,9 @@
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 my_dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
 
+my_dic1 = {'name': 'John', 'age': 25, 'city': 'New York'}
+print(my_dic1['city'])
+
 for i in my_list:
     print(i)
     
