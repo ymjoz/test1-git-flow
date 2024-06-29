eb1f6fe185710bc0dc9df31945313fbe9cf04ceb
ymjoz <rossiblack@duck.com>
2024-06-29 21:36:09 +0800

    可同時處理svg與png

--- url_snap.py
+++ url_snap.py
@@ -2,8 +2,9 @@ import requests
 from PIL import Image
 from io import BytesIO
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 import cairosvg
+import os
 
 ### 1. 通过url获取图片
 
@@ -11,7 +12,8 @@ import cairosvg
 # url = "https://www.u-tokyo.ac.jp/en/about/office_president.html"
 # base_url = "https://www.u-tokyo.ac.jp/en/"
 # base_url = "https://www.kyoto-u.ac.jp/en"
-base_url = "https://www.hokudai.ac.jp/"
+# base_url = "https://www.hokudai.ac.jp/"
+base_url = "https://www.keio.ac.jp/ja/"
 response = requests.get(base_url)
 soup = BeautifulSoup(response.text, 'html.parser')
 img_tag = soup.find('img', )  # 需要替换为实际的图片标签和类名
@@ -21,15 +23,27 @@ if img_tag:
     full_img_url = urljoin(base_url, image_url)
     print(full_img_url) # https://www.u-tokyo.ac.jp/content/400215559.svg
 
-    img_name = 'hokudai2' + '.png'
+    parsed_url = urlparse(full_img_url)
+    file_path = parsed_url.path
+    file_name = os.path.basename(file_path)
+    _, file_extension = os.path.splitext(file_name)
+    
+    img_name = file_name
    
     # FIXME: 如果原圖是png格式，則不需要轉換，直接保存即可 
     try:
-        cairosvg.svg2png(url=full_img_url, write_to=img_name)  # 保存图片到当前目录下
-        image = Image.open(img_name)
-        image.save(img_name)  # 保存图片到当前目录下
+        if file_extension == ".svg":
+            cairosvg.svg2png(url=full_img_url, write_to=img_name)  # 保存图片到当前目录下
+            image = Image.open(img_name)
+            image.save(img_name)
+        elif file_extension == ".png":
+            response = requests.get(full_img_url)
+            image = Image.open(BytesIO(response.content))
+            image.save(img_name)
+        else:
+            print("Image format not supported.") 
     except:
-        print("Image png will not be converted.")
+        print("Image format unknown.") 
     
 else:
     print("Image not found.")
